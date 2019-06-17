6013c64e876598f02b8755993054e9b91e4ee419
Rodolfo Galarza Jr <rodolfoxgalarza@gmail.com>
2019-06-16 23:26:11 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Rodolfo Galarza on 6/16/19.
+//  Copyright © 2019 Rodolfo Galarza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
