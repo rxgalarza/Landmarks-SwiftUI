2b07dd112aef2a2920c2b9ff15059e931b012671
Rodolfo Galarza Jr <rodolfoxgalarza@gmail.com>
2019-06-17 15:42:13 -0500

    A main view with three components 	- MapView 	- CircleImage 	- Text

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,7 +10,33 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    HStack {
+                        Text("Joshua Tree National Park")
+                            .font(.subheadline)
+                            Spacer()
+                            Text("California")
+                                .font(.subheadline)
+                        }
+                    }
+                    .padding()
+                    Spacer()
+            }
+       
+        
     }
 }
 
